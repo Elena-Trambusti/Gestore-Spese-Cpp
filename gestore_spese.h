18e85cf7f93a9a3963e9c4c354b1f416b08edbf4
Elena-Trambusti <elenatrambusti2020@gmail.com>
2026-03-12 12:01:22 +0100

    Update gestore_spese.h

--- gestore_spese.h
+++ gestore_spese.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
@@ -12,12 +13,10 @@ struct spesa {
     string nome;
     float importo;
 };
-
-// 2. I "TITOLI" DELLE TUE FUNZIONI (Dichiarazioni)
 void mostraMenu();
-void visualizzaSpese(spesa listaSpese[], int contatoreSpese);
-void aggiungiSpesa(spesa listaSpese[], int &contatoreSpese, float &totaleSpese);
-void controllaSaldo(float budgetIniziale, float totaleSpese);
-void salvaDati(spesa listaSpese[], int contatoreSpese, float budgetIniziale);
+void visualizzaSpese(vector<spesa>&listaSpese);
+void aggiungiSpesa(vector<spesa>&listaSpese,float &totaleSpese);
+void controlloSaldo(float budgetIniziale,float totaleSpese);
+void salvaDati(vector<spesa> &listaSpese, float budgetIniziale);
 
 #endif
