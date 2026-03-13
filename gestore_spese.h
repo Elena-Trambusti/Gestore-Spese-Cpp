5f57f6a0f1e24f8ad56adb35089a5fd5b0b3d97d
Elena-Trambusti <elenatrambusti2020@gmail.com>
2026-03-13 11:26:15 +0100

    Implementazione metodi Getter/Setter e logica OPP in gestione_spese.cpp
    Aggiornata l'implementazione delle funzioni per riflettere il passaggio dalla struct alla classe Spesa:
    
    Sostituito l'accesso diretto ai membri (.nome, .importo) con i metodi protetti (.getNome(), .getImporto()).
    
    Aggiornata la funzione 'aggiungiSpesa' per utilizzare il costruttore della classe, garantendo l'integrità dei dati fin dalla creazione.
    
    Allineate le firme delle funzioni al nuovo standard di nomenclatura (S maiuscola).

--- gestore_spese.h
+++ gestore_spese.h
@@ -8,15 +8,41 @@
 
 using namespace std;
 
-// 1. LA TUA STRUTTURA DATI
-struct spesa {
-    string nome;
-    float importo;
+class Spesa{
+private:
+string nome;
+float importo;
+
+public:
+//costruttore vuoto
+Spesa(){
+nome="";
+importo=0.0;
+}
+//costruttore con i dati
+Spesa(string n, float i){
+    nome=n;
+    importo=i;
+}
+//I GETTER (per leggere in sicurezza)
+string getNome()const{return nome;}
+float getImporto()const{retunr importo;}
+
+//I SETTER (per scrivere in sicurezza)
+void setNome(string n){nome=n;}
+void setImporto(float i){
+if(i>0){
+importo=i;
+}else{
+importo=0; //se c'e' un errore, forzo a zero
+}
+}
 };
+
 void mostraMenu();
-void visualizzaSpese(vector<spesa>&listaSpese);
-void aggiungiSpesa(vector<spesa>&listaSpese,float &totaleSpese);
+void visualizzaSpese(vector<Spesa>&listaSpese);
+void aggiungiSpesa(vector<Spesa>&listaSpese,float &totaleSpese);
 void controlloSaldo(float budgetIniziale,float totaleSpese);
-void salvaDati(vector<spesa> &listaSpese, float budgetIniziale);
+void salvaDati(vector<Spesa> &listaSpese, float budgetIniziale);
 
 #endif
